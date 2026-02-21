57776c2d01f7677eab9fa9fb266d4e900f85bb23
garyfm <garyfmullen@gmail.com>
2026-02-21 17:15:58 +0000

    Add deferred allocation to GlBuffer

--- libinneall/renderer/gl_buffer.cpp
+++ libinneall/renderer/gl_buffer.cpp
@@ -4,14 +4,43 @@
 
 namespace inl {
 
+GlBuffer::GlBuffer(std::size_t size)
+    : m_size { size } {
+
+    create_buffer(nullptr, size);
+}
+
 GlBuffer::GlBuffer(std::span<std::byte const> data)
     : m_size { data.size() } {
 
-    // Allocate a buffer handle
-    glCreateBuffers(1, &m_handle);
+    create_buffer(data.data(), data.size());
+}
 
-    // Allocate a buffer storage
-    glNamedBufferStorage(m_handle, data.size(), data.data(), GL_DYNAMIC_STORAGE_BIT);
+void GlBuffer::allocate(std::span<std::byte const> data) {
+    m_handle.reset();
+    m_size = 0;
+
+    create_buffer(data.data(), data.size());
+
+    m_size = data.size();
+}
+
+void GlBuffer::allocate(std::size_t size) {
+    m_handle.reset();
+    m_size = 0;
+
+    create_buffer(nullptr, size);
+
+    m_size = size;
+}
+
+void GlBuffer::upload(std::size_t offset, std::span<std::byte const> data) {
+    glNamedBufferSubData(m_handle, offset, data.size(), data.data());
+}
+
+void GlBuffer::create_buffer(std::byte const* data, std::size_t size) {
+    glCreateBuffers(1, &m_handle);
+    glNamedBufferStorage(m_handle, size, data, GL_DYNAMIC_STORAGE_BIT);
 }
 
 void GlBuffer::delete_buffer(GLuint buffer) { glDeleteBuffers(1, &buffer); }
--- libinneall/renderer/gl_buffer.hpp
+++ libinneall/renderer/gl_buffer.hpp
@@ -10,16 +10,22 @@ namespace inl {
 class GlBuffer {
 public:
     GlBuffer() = default;
+    explicit GlBuffer(std::size_t size);
     explicit GlBuffer(std::span<std::byte const> data);
+
+    void allocate(std::size_t size);
+    void allocate(std::span<std::byte const> data);
+    void upload(std::size_t offset, std::span<std::byte const> data);
 
     GLuint native_handle() const { return m_handle; }
     std::size_t size() const { return m_size; };
 
 private:
+    void create_buffer(std::byte const* data, std::size_t size);
     static void delete_buffer(GLuint buffer);
 
     UniqueResource<GLuint, decltype(&delete_buffer)> m_handle { 0, delete_buffer };
-    std::size_t m_size;
+    std::size_t m_size { 0 };
 };
 
 } // namespace inl

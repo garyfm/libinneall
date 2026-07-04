d9d23f550a4455c76f5822622a14bd984645d60f
garyfm <garyfmullen@gmail.com>
2026-07-04 20:21:46 +0100

    platform: Replace glfw with platform layer

--- libinneall/window.hpp
+++ libinneall/window.hpp
@@ -4,19 +4,18 @@
 #include <libinneall/base/string.hpp>
 #include <libinneall/base/unique_ptr.hpp>
 #include <libinneall/base/utility.hpp>
+#include <libinneall/platform/platform.hpp>
 
-#define GLFW_INCLUDE_NONE
 #include <subprojects/glad/include/glad/glad.h>
-#include <subprojects/glfw-3.4/include/GLFW/glfw3.h>
 
 namespace inl {
 
 class Window {
 public:
-    using InputCallback = void (*)(GLFWwindow*);
-    using MouseCallback = void (*)(GLFWwindow*, double, double);
-    using ScrollCallback = void (*)(GLFWwindow*, double, double);
-    using ResizeCallback = void (*)(GLFWwindow*, int32_t, int32_t);
+    using InputCallback = void (*)(platform::Window&);
+    using MouseCallback = void (*)(platform::Window&, double, double);
+    using ScrollCallback = void (*)(platform::Window&, double, double);
+    using ResizeCallback = void (*)(platform::Window&, int32_t, int32_t);
 
     Window() = default;
     ~Window();
@@ -25,7 +24,7 @@ public:
     static Error create(Window& window, uint32_t width, uint32_t height, StringView title, InputCallback input_callback,
         MouseCallback mouse_callback, ScrollCallback scroll_callback, ResizeCallback resize_callback);
 
-    GLFWwindow* handle() const { return m_window.get(); }
+    // GLFWwindow* handle() const { return m_window.get(); }
     uint32_t width() const { return m_width; }
     uint32_t height() const { return m_height; }
     float aspect_ratio() const { return static_cast<float>(m_width) / static_cast<float>(m_height); }
@@ -41,8 +40,9 @@ private:
     uint32_t m_height { 0 };
     String<MAX_TITLE_SIZE> m_title {};
 
-    static void framebuffer_resize_callback([[maybe_unused]] GLFWwindow* window, int32_t width, int32_t height);
-    UniquePtr<GLFWwindow, glfwDestroyWindow> m_window { nullptr };
+    static void framebuffer_resize_callback([[maybe_unused]] platform::Window& window, int32_t width, int32_t height);
+    // UniquePtr<GLFWwindow, glfwDestroyWindow> m_window { nullptr };
+    platform::Window m_native_window;
 
     // TODO: Temporary solution to allow game to handle input
     InputCallback m_input_callback;
